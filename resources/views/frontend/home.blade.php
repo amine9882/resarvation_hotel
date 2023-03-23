1113e6d7e62511203f5234a4883bf19b2dcfc81c
amine9882 <aminemaameri31@gmail.com>
2023-03-23 11:57:32 +0100

    commit 3

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,54 @@
+@section('title')
+    welcome to Shope
+@endsection
+
+@section('content')
+     @include('layouts.inc.slider')
+     @extends('layouts.app')
+
+     <hr>
+     
+    {{-- <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                   
+                 
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+       
+                            </div>
+                        @endif
+    
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <h2>Claviers, souris </h2>
+                {{-- <div class="owl-carousel featured-carousel owl-theme element1"> --}}
+                    @foreach ($featured_products as $prod)
+                        <div class="col-md-3 mt-3">
+                            <div style="height: 600px" class="card">
+                                <img src="{{asset('assets/uploads/product/'.$prod->image)}}" height="200px" class="card-img-top" alt="product">
+                                <div class="card-body">
+                                  <h5 class="card-title">{{$prod->name}}</h5>
+                                  <p class="card-text">{{$prod->description}}</p>
+                                  <span class="float-start">{{$prod->selling_price}}DZ</span>
+                                  <span class="float-end"><s>{{$prod->original_price}}DZ</s></span>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                {{-- </div> --}}
+               
+            </div>
+        </div>
+    </div>
+@endsection
